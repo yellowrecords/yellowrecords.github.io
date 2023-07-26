d7b135ea621948df3a827951b2e1f568e7f78256
Yellow Records <122419789+yellowrecords@users.noreply.github.com>
2023-07-26 15:57:14 -0400

    Bobby Nair rewrite

--- js/global.js
+++
@@ -1,3 +0,0 @@
-function pageLoad() {
-  document.getElementById('copyright').appendChild(document.createTextNode("\u00a9 Yellow Records " + new Date().getFullYear()))
-}
---
+++ old/js/global.js
@@ -0,0 +1,3 @@
+function pageLoad() {
+  document.getElementById('copyright').appendChild(document.createTextNode("\u00a9 Yellow Records " + new Date().getFullYear()))
+}
